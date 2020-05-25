90f7c261f1cc23157a4a9be377b990963927bebf
unknown <kobimamuka7@gmail.com>
2020-05-25 13:56:23 +0300

    'dep'

--- module/app.js
+++ module/app.js
@@ -299,7 +299,6 @@ const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static("/build"));
-	app.get('*', (req, res)=>{res.sendFile(path.resolve(__dirname, 'build', 'index.html'));})
 }
 
 app.listen(PORT, ()=>
